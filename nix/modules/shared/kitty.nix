d7ef52bcead0277f4508d472bbce84fb73541f66
Isaac Dadzie <4581114+idadzie@users.noreply.github.com>
2024-11-24 23:16:42 +0000

    Add kitty configuration

--- nix/modules/shared/kitty.nix
+++ nix/modules/shared/kitty.nix
@@ -2,6 +2,7 @@
   pkgs,
   lib,
   config,
+  options,
   ...
 }: let
   cfg = config.my.modules.kitty;
@@ -17,32 +18,34 @@ in {
 
   config = with lib;
     mkIf cfg.enable (mkMerge [
-      (mkIf isDarwin {
-        homebrew.casks = ["kitty"];
-        my = {
-          env = {
-            TERMINFO_DIRS = [
-              "$KITTY_INSTALLATION_DIR/terminfo"
-            ];
+      (
+        if (builtins.hasAttr "homebrew" options)
+        then {
+          homebrew.casks = ["kitty"];
+          my = {
+            env = {
+              TERMINFO_DIRS = [
+                "$KITTY_INSTALLATION_DIR/terminfo"
+              ];
+            };
           };
-        };
-      })
-      (mkIf isLinux {
-        my = {
-          user = {
-            packages = with pkgs; [
-              kitty
-              # kitty-themes
-            ];
-          };
+        }
+        else {
+          my = {
+            user = {
+              packages = with pkgs; [
+                kitty
+              ];
+            };
 
-          env = {
-            TERMINFO_DIRS = [
-              "${pkgs.kitty.terminfo}/share/terminfo"
-            ];
+            env = {
+              TERMINFO_DIRS = [
+                "${pkgs.kitty.terminfo}/share/terminfo"
+              ];
+            };
           };
-        };
-      })
+        }
+      )
 
       {
         my = {
